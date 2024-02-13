db6f2d5eeff975a1a3fb0c101f812dd814123b50
Kush Agarwal <agarwalkushajay@gmail.com>
2024-02-13 21:56:23 +0530

    Modified DBConnect.java

--- src/db/DBConnect.java
+++ src/db/DBConnect.java
@@ -4,10 +4,21 @@
  */
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author kushhhh
  */
 public class DBConnect {
-    
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:records.db");
+            return conn;
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }    
 }
