cd5b8da5701fca60fb765c795acdda6b944b087e
Kush Agarwal <agarwalkushajay@gmail.com>
2024-02-13 22:01:57 +0530

    Fixed Some Error in DBConnect

--- src/db/DBConnect.java
+++ src/db/DBConnect.java
@@ -19,6 +19,7 @@ public class DBConnect {
             return conn;
         } catch(SQLException e) {
             System.out.println(e.getMessage());
+            return null;
         }
     }    
 }
