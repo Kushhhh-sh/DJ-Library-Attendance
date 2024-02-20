fd8abbb8c2674a992c7344d99ab9f9e7a5e35a7c
Kush Agarwal <agarwalkushajay@gmail.com>
2024-02-20 21:12:19 +0530

    Changed the Connection to static

--- src/db/DBConnect.java
+++ src/db/DBConnect.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,22 +18,32 @@ import java.sql.Statement;
 public class DBConnect {
     private static final String STUDENTS_TABLE_SQL;
     private static final String FACULTY_TABLE_SQL;
-    
+    private static Connection conn = null;
+
     static {
         STUDENTS_TABLE_SQL = "CREATE TABLE IF NOT EXISTS students (" +
                 "sap_id TEXT, date TEXT, in_time TEXT, out_time TEXT, status TEXT, PRIMARY KEY(sap_id, date, in_time))";
         FACULTY_TABLE_SQL = "CREATE TABLE IF NOT EXISTS faculty (" +
                 "sap_id TEXT, date TEXT, in_time TEXT, out_time TEXT, status TEXT, PRIMARY KEY(sap_id, date, in_time))";
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:records.db");
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public static Connection getConnection() {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:records.db");
-            try (Statement stmt = conn.createStatement()) {
-                stmt.execute(STUDENTS_TABLE_SQL);
-                stmt.execute(FACULTY_TABLE_SQL);
+            if(conn != null) {
+                try (Statement stmt = conn.createStatement()) {
+                    stmt.execute(STUDENTS_TABLE_SQL);
+                    stmt.execute(FACULTY_TABLE_SQL);
+                }
+                return conn;
+            } else {
+                return null;
             }
-            return conn;
+                
         } catch(SQLException e) {
             System.out.println(e.getMessage());
             return null;
