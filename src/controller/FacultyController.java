b12175bf841a2c41cd75f127484cbabf608abc4c
Kush Agarwal <agarwalkushajay@gmail.com>
2024-02-20 15:54:41 +0530

    Forced To make CSV File

---
+++ src/controller/FacultyController.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import db.DBConnect;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author kushhhh
+ */
+public class StudentController {
+    private Connection conn;
+    private PreparedStatement statement;
+    
+    public StudentController() {
+        conn = DBConnect.getConnection();
+        
+    }
+    
+    public ResultSet getStudentsWithinDateRange(String from, String to) {
+        ResultSet rs;
+        try {
+            this.statement = this.conn.prepareStatement("SELECT * FROM students WHERE date >= ? AND date <= ?");
+            this.statement.setString(1, from);
+            this.statement.setString(2, to);
+            rs = this.statement.executeQuery();
+            return rs;
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentController.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public void insert(String sapId, String date, String time) {
+        boolean isIn = false;
+        String inTime = "";
+        String todayDate = DateTimeFormatter.ofPattern("yyyy/MM/dd").format(LocalDateTime.now());
+                
+        try {
+            statement = this.conn.prepareStatement("SELECT sap_id, date, in_time, status FROM students WHERE sap_id = ? AND date = ?");
+            statement.setString(1, sapId);
+            statement.setString(2, date);
+            
+            ResultSet rs = statement.executeQuery();
+            
+            if(rs != null) {
+                while(rs.next()) {
+                    if("in".equals(rs.getString(4)) && todayDate.equals(rs.getString(2))){
+                        isIn = true; 
+                        inTime = rs.getString(3);
+                        System.out.println(isIn + " - " + inTime);
+                    }
+                }
+            } 
+            
+            if(! isIn) {
+                System.out.println("Inserting record for " + sapId);
+                statement = conn.prepareStatement("INSERT INTO students(sap_id, date, in_time, status) VALUES (?, ?, ?, ?)");
+                statement.setString(1, sapId);
+                statement.setString(2, date);
+                statement.setString(3, time);
+                statement.setString(4, "in");
+                statement.execute();
+            } else {
+                statement = conn.prepareStatement("UPDATE students SET out_time = ?, status = ? WHERE sap_id = ? AND in_time = ?");
+                statement.setString(1, time);
+                statement.setString(2, "out");
+                statement.setString(3, sapId);
+                statement.setString(4, inTime);
+                statement.execute();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
